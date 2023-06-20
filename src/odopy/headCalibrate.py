c63bde809e3d69fc33605cc4ae82c1527ac30bac
bszek213 <bszekely@nevada.unr.edu>
2023-06-20 13:50:22 -0700

    FIX: yaml read in issue again again again

--- src/odopy/headCalibrate.py
+++ src/odopy/headCalibrate.py
@@ -64,8 +64,6 @@ class headCalibrate():
             print('Found odo_times.yaml.')
             with open(path_odo) as file:
                 time_list = yaml.load(file, Loader=yaml.FullLoader)
-            print(time_list[0])
-            input()
             self.pitch_start = time_list[0]['calibration']['pitch_start']
             self.pitch_end = time_list[0]['calibration']['pitch_end']
             self.yaw_start = time_list[0]['calibration']['yaw_start']
